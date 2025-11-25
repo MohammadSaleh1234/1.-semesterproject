39101f25bd28fb5d73891d3975ba3ce0f0d7e130
HegarMohamad <hegarsaleh/@gmail.com>
2025-11-25 14:45:43 +0100

    tilføjet lidt hist og pist

--- StartRoom.cs
+++ StartRoom.cs
@@ -7,7 +7,7 @@ public class StartRoom : Space
     public override void Welcome()
     {
         var spaceName = GetName();
-        string line = "You are now at the "+name;
+        string line = "Go to the beach and see what tasks the Ocean code brings you today!";
 
         int windowWidth = Console.WindowWidth;
         int x = ((Console.WindowWidth - line.Length) / 2);
@@ -18,7 +18,9 @@ public class StartRoom : Space
 
         var exits = edges.Keys.ToHashSet();
         Console.SetCursorPosition(x, Console.CursorTop);
-        Console.WriteLine("You can continue to: ");
+        Console.WriteLine("Type 'help' if you need a little assistance moving around");
+        Console.SetCursorPosition(x, Console.CursorTop);
+        Console.WriteLine("if you need furthur instructions on your current tasks then you can type type 'logbook', your curent exist are:");
         foreach (var exit in exits) {
             Console.SetCursorPosition(x, Console.CursorTop);
             Console.WriteLine(" - " + exit);}
